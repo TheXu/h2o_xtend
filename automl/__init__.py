77b5a6be9afff01889425a4b662de59a5ca459d1
xu <xua@coned.com>
2019-05-07 17:30:13 -0400

    Added __init__ for automl folder

---
+++ automl/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+h2o-xtend Machine Learning Library Extensions
+Created on 2019
+
+@author: Alex Xu <ayx2@case.edu>
+"""
+from .hyper_opt import hyper_opt
+
+__all__ = [
+    'hyper_opt'
+]
